580aeec44d8fbf9193b34bd1975e86605ea66104
Damian <diazdamian@gmail.com>
2025-07-10 20:52:49 -0300

    dotenv

--- index.js
+++ index.js
@@ -20,5 +20,6 @@ app.use((req, res, next) => {
   res.status(404).json({ error: "Not Found" });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
