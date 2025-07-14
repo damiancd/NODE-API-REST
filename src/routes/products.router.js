9e5c441e9e0c3f555f3f14cee087d82a646b2034
Damian <diazdamian@gmail.com>
2025-07-14 20:23:37 -0300

    corrijo ruta create

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,14 +2,6 @@ import { Router } from "express";
 
 const router = Router();
 
-// const products = [
-//   { id: 1, name: "Camiseta Deportiva", price: 150 },
-//   { id: 2, name: "Zapatos Running", price: 1200 },
-//   { id: 3, name: "Mochila Escolar", price: 350 },
-//   { id: 4, name: "Auriculares Bluetooth", price: 800 },
-//   { id: 5, name: "Botella Térmica", price: 220 },
-// ];
-
 import {
   getAllProducts,
   searchProduct,
@@ -24,7 +16,7 @@ router.get("/products", getAllProducts);
 router.get("/products/search", searchProduct);
 router.get("/products/:id", getProductById);
 
-router.post("/products", createProduct);
+router.post("/products/create", createProduct);
 
 router.put('/products/:id', updateProduct);
 
